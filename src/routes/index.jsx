ae6472d8626d37207f88ef5d55b4e704d98f18f4
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-03-13 09:11:36 -0300

    feat: utilized botton navigation

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 
 import StackRoutes from "./stack.routes";
+import TabRoutes from "./tab.routes";
+import DrawerRoutes from "./drawer.routes";
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {/*<StackRoutes />*/}
+      <TabRoutes />
+      {/* <DrawerRoutes /> */}
       <StatusBar style="auto" />
     </NavigationContainer>
   );
